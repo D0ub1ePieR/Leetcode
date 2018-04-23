982e13890282cc1c68673028481c279e8f200ba9
D0ub1ePieR <37244096+D0ub1ePieR@users.noreply.github.com>
2018-04-23 19:43:54 +0800

    figure out Problem15

---
+++ LeetCodePython/15-三数之和.py
@@ -0,0 +1,28 @@
+class Solution:
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res=[]
+        nums.sort()
+        for i in range(len(nums)):
+            if i==0 or nums[i-1]!=nums[i]:
+                j=i+1
+                k=len(nums)-1
+                while j<k:
+                    s=nums[i]+nums[j]+nums[k]
+                    if s==0:
+                        res.append([nums[i],nums[j],nums[k]])
+                        j+=1
+                        k-=1
+                        while j<k and nums[j]==nums[j-1]:
+                            j+=1
+                        while j<k and nums[k+1]==nums[k]:
+                            k-=1
+                    elif s>0:
+                        k-=1
+                    else:
+                        j+=1
+        return res
+            
