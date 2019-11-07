953eb6b4602320e5d9b749a008cb033d92fe75ee
D0ub1ePieR <535447508@qq.com>
2019-11-07 20:17:41 +0800

    [update] 更新题目列表

--- script.py
+++ script.py
@@ -2,15 +2,36 @@ import glob
 
 path = './LeetCodePython/common/'
 files = glob.glob(path + '*.py')
+
+rd_path = './README.md'
+f = open(rd_path, 'r', encoding='UTF-8')
+lines = f.readlines()
+f.close()
+
+f = open(rd_path, 'w', encoding='UTF-8')
+for line in lines:
+    f.write(line)
+    if line.find(':-:') != -1:
+        break
+
 for file in files:
+    index = file.index('\\')
+    filename = file[index+1:-3]
+    index = filename.index('-')
+    num = filename[:index]
+    name = filename[index+1:]
+    f.write('| ' + num + ' | ' + name + ' |\n')
     pass
 
+f.close()
+'''
 wp_path = './WriteUp.md'
 f = open(wp_path, 'r', encoding='UTF-8')
 lines = f.readlines()
 f.close()
 
-f = open(wp_path, 'w')
+f = open(wp_path, 'w', encoding='UTF-8')
 for line in lines:
     f.write(line)
 f.close()
+'''
