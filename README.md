c10077f5dee592b3b9e6ff6b33f2b652c7cdd718
D0ub1ePieR <535447508@qq.com>
2020-02-16 01:38:13 +0800

    [update] 添加题目列表; [add] 添加生成列表脚本

--- README.md
+++ README.md
@@ -3,7 +3,14 @@
 ***
 <start-tag>
 
-当前进度:  **0 / -**
+当前进度:  **5 / -**
+| No. | 题目名称 | Language | 难度 | tag | 运行速度(超过) | 占用内存(超过) |
+| :-: | :-: | :-: | :-: | :-: | :-: | :-: |
+| 1 | [两数之和](./solutions/1-Two_Sum-两数之和) | python3 | simple | 哈希表 数组 | 73.87% | 18.46% |
+| 15 | [三数之和](./solutions/15-3Sum-三数之和) | python3 | medium | 数组 双指针 | 99.15% | 20.33% |
+| 16 | [最接近的三数之和](./solutions/16-3Sum_Closest-最接近的三数之和) | python3 | medium | 数组 双指针 | 51.14% | 48.75% |
+| 167 | [两数之和Ⅱ_输入有序数组](./solutions/167-Two_SumII_Input_array_is_sorted-两数之和Ⅱ_输入有序数组) | python3 | simple | 双指针 数组 二分查找 | 73.28% | 48.12% |
+| 653 | [两数之和IV_输入BST](./solutions/653-Two_Sum_IV_Input_is_a_BST-两数之和IV_输入BST) | python3 | simple | 树 | 90.45% | 37.64% |
 
 <end-tag>
 
---
+++ script.py
@@ -0,0 +1,65 @@
+import glob
+import os
+
+
+def get_name(st):
+    while st.find('-') != -1:
+        name_index = st.find('-')
+        st = st[name_index+1:]
+    return st
+
+
+def get_info(st):
+    py_file_name = glob.glob(path + st + '/*.py')[0]
+    py_file = open(py_file_name, 'r', encoding='utf-8')
+
+    for j in range(5):
+        line = py_file.readline()
+        if j == 0:
+            language = line[2:-1]
+        if j == 1:
+            level = line[2:-1]
+        if j == 2:
+            tag = line[2:-1]
+        if j == 3:
+            speed = line[-7:-1]
+        if j == 4:
+            memory = line[-7:-1]
+
+    return language, level, tag, speed, memory
+
+
+path = './solutions/'
+file_name = './readme.md'
+
+file = open(file_name, 'r', encoding='utf-8')
+lines = file.readlines()
+file.close()
+start = lines.index('<start-tag>\n')
+end = lines.index('<end-tag>\n')
+
+dic = {}
+for root, dirs, files in os.walk(path):
+    for pack in dirs:
+        index = pack.index('-')
+        dic[int(pack[:index])] = pack
+
+nums = sorted(dic)
+
+file = open(file_name, 'w', encoding='utf-8')
+for i in range(start+2):
+    file.write(lines[i])
+file.write('当前进度:  **' + str(len(nums)) + ' / -**\n')
+file.write('| No. | 题目名称 | Language | 难度 | tag | 运行速度(超过) | 占用内存(超过) |\n')
+file.write('| :-: | :-: | :-: | :-: | :-: | :-: | :-: |\n')
+
+for num in nums:
+    no = num
+    name = get_name(dic[num])
+    language, level, tag, speed, memory = get_info(dic[num])
+    url = path + dic[num]
+    file.write('| %s | [%s](%s) | %s | %s | %s | %s | %s |\n' % (str(no), name, url, language, level, tag, speed, memory))
+
+for i in range(end-1, len(lines)):
+    file.write(lines[i])
+file.close()
